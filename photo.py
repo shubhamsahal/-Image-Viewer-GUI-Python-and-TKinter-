01c7430aa0e7dcd9c144466ff90b6c5a0276a97e
Shub <97597184+shubnet@users.noreply.github.com>
2023-02-20 18:20:14 +0530

    Update photo.py

--- photo.py
+++ photo.py
@@ -18,9 +18,9 @@ my_label.grid(row=0,column=0,columnspan=3)
 
 
 def forward(image_number):
-    global my_label                                            # video - https://www.youtube.com/watch?v=zg4c92pNFeo
-    global button_forward                                       # time stam : 15:44 
-    global button_back                                           # forward button
+    global my_label                                           
+    global button_forward                                       
+    global button_back                                           
     
     my_label.grid_forget()
     my_label = Label(image=image_list[image_number-1])
